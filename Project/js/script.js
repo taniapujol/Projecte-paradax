23ae071db6948671498b51bdd77f78e78ba80976
SURFACE\ainat <ainatpujolrigual@gmail.com>
2017-02-05 12:39:25 +0100

    Primera part finalitzada

--- Project/js/script.js
+++ Project/js/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
-
 	//Ajustamos el div sroll al final de la página
-		var height = $(window).height();
+		var height = $(window).height(),
+				final;
 		ajustesiniciales();
 		function ajustesiniciales() {
-			$("#etiqueta_scroll").css({"margin-top":height - 80 + "px"});
+			$("#etiqueta_scroll").css({"margin-top":height - 70 + "px"});
+			$("#logo").css({"margin-top": "150px", "width": "350px","height":"350px"});
+			$("header").css({"background-color": "transparent"});
+			$("nav a").css({"color":"transparent"});
+		};
 
-		}
 // Aplicamos el efecto scroll de transición del icono al menu
 	var flag = false;
 	var scroll;
@@ -17,7 +20,7 @@ $(document).ready(function(){
 		if(scroll > 200){
 			if(!flag){
 				$("#logo").css({"margin-top": "-5px", "width": "50px","height":"50px"});
-				$("header").css({"background-color": "#000"});
+				$("header").css({"background-color": "transparent"});
 				$("nav a").css({"color":"#fff"});
 				flag = true;
 			}
@@ -30,10 +33,8 @@ $(document).ready(function(){
 				flag = false;
 			}
 		}
-
-
 	});
-	(document).scroll(function(){
+	$(document).scroll(function(){
 			var scrollTop = $(this).scrollTop();
 			var pixels = scrollTop / 70;
 
@@ -46,5 +47,4 @@ $(document).ready(function(){
 			}
 
 		});
-
 });
